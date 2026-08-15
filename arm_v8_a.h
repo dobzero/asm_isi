ea51d54a25679edde8e78a7963ddae9383d64ae0
callsvc <gabriel_correia0202@outlook.com>
2026-08-15 09:58:55 -0300

    (asm_isi), adds __MRS instructions variant

--- arm_v8_a.h
+++ arm_v8_a.h
@@ -8,7 +8,16 @@ class ArmV8_A : protected FixedBufferSizeEmitter<uint32_t> {
 public:
     // https://www.scs.stanford.edu/~zyedidia/arm64/nop.html
     uint32_t NOP() {
-        return emitter(0xD503201F);
+        return emitter(__NOP());
+    }
+
+    static uint32_t __MRS(const uint8_t rt, const uint8_t op0, const uint8_t op1, const uint8_t crn, const uint8_t crm, const uint8_t op2) {
+        return 0xD5200000 |
+            (op0 << 19) | (op1 << 16) | (crn << 12) | (crm << 8) | (op2 << 5) | rt;
+    }
+
+    static uint32_t __NOP() {
+        return 0xD503201F;
     }
     [[nodiscard]] auto get_asm() const {
         return std::span{reinterpret_cast<const uint8_t *>(asm_buffer_stream.data()), asm_buffer_stream.size()};
